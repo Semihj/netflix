18c99c11025606a0f87c711597d9c1df2c32b6dc
Semihj <shouldahjdh@gmail.com>
2023-05-10 02:38:09 +0300

    modified:   src/components/Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -55,7 +55,7 @@ const Main = () => {
         <div className="absolute w-full top-[20%] p-4 md:p-8 ">
           <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
           <div className="my-4">
-            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5 hover:bg-gray-800 hover:text-white hover:scale-[1.1] transition-all">
+            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5 hover:bg-transparent hover:text-white hover:scale-[1.1] transition-all">
               Play
             </button>
             <button onClick={saveShow} className="border  text-white border-gray-300 py-2 px-5 ml-4 hover:bg-gray-300 hover:text-black hover:scale-[1.13] transition-all">
