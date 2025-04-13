21de918c6583962dcf69c16ab1f62f9a16310726
Semih <shouldahjdh@gmail.com>
2025-04-13 07:02:59 +0300

    added google sign in to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,71 +1,106 @@
-import  { useState } from 'react'
-import bgimage from "../assets/netflixbg.jpg"
-import { Link,useNavigate } from 'react-router-dom'
-import { UserAuth } from '../context/AuthContext'
+import { useEffect, useState } from "react";
+import bgimage from "../assets/netflixbg.jpg";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+import { AiFillGooglePlusCircle } from "react-icons/ai";
+
 const Login = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
-  const {logIn} = UserAuth()
-  const navigate = useNavigate()
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { logIn } = UserAuth();
+  const navigate = useNavigate();
+  const { user, handleGoogle } = UserAuth();
+  useEffect(() => {
+   if (user) {
+    navigate("/")
+   }
+  }, [user])
+  
+  
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    setError("")
+    e.preventDefault();
+    setError("");
     try {
-      await logIn(email,password)
-      navigate("/")
-
+      await logIn(email, password);
+      navigate("/");
     } catch (error) {
-      console.log(error)
-      setError(error.message)
+      console.log(error);
+      setError(error.message);
     }
-  }
+  };
+  const handleGoogleSign = async (e) => {
+    e.preventDefault();
+    try {
+      await handleGoogle(user);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-    <div className="w-full h-screen">
-      <img
-        className="hidden sm:block absolute w-full h-full object-cover "
-        src={bgimage}
-        alt=""
-      />
-      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen">
-        <div className="fixed w-full px-4 py-24 z-50">
-          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white ">
-            <div className="max-w-[320px] mx-auto py-16 ">
-              <h1 className="text-3xl font-bold ">Sign  In</h1>
-              {error ? <p className='p-3 bg-red-400 my-2'>{error}</p>:null}
-              <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="p-3 my-2 bg-gray-700 rounded"
-                  type="email"
-                  placeholder="Email"
-                  autoComplete="email"
-                />
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="p-3 my-2 bg-gray-700 rounded "
-                  type="password"
-                  placeholder="Password"
-                  autoComplete="current-password"
-                />
-                <button className="bg-red-600 py-3 my-6 rounded font-bold ">
-                  Sign In
-                </button>
-                <div className="flex items-center justify-between text-sm text-gray-600">
-                  <p><input className="mr-2" type="checkbox" />Remember me</p>
-                  <p>Need Help?</p>
-                </div>
-                <p className="py-8 "><span className="text-gray-600">New to Netflix? </span><Link to="/signup">Sign Up</Link></p>
-              </form>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover "
+          src={bgimage}
+          alt=""
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen">
+          <div className="fixed w-full px-4 py-24 z-50">
+            <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white ">
+              <div className="max-w-[320px] mx-auto py-16 ">
+                <h1 className="text-3xl font-bold ">Sign In</h1>
+                {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
+                <form
+                  onSubmit={handleSubmit}
+                  className="w-full flex flex-col py-4"
+                >
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="p-3 my-2 bg-gray-700 rounded"
+                    type="email"
+                    placeholder="Email"
+                    autoComplete="email"
+                  />
+                  <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="p-3 my-2 bg-gray-700 rounded "
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="current-password"
+                  />
+                  <button className="bg-red-600 py-3 my-6 rounded font-bold ">
+                    Sign In
+                  </button>
+                  <div className="flex items-center justify-between text-sm text-gray-600">
+                    <p>
+                      <input className="mr-2" type="checkbox" />
+                      Remember me
+                    </p>
+                    <p>Need Help?</p>
+                  </div>
+                  <div className="w-full flex justify-center ">
+                    <button
+                      type="button"
+                      onClick={handleGoogleSign}
+                      className="flex items-center gap-3 mt-5 "
+                    >
+                      Sign in with Google <AiFillGooglePlusCircle size={30} />{" "}
+                    </button>
+                  </div>
+                  <p className="py-8 ">
+                    <span className="text-gray-600">New to Netflix? </span>
+                    <Link to="/signup">Sign Up</Link>
+                  </p>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
-  )
-}
+    </>
+  );
+};
 
-export default Login
+export default Login;
